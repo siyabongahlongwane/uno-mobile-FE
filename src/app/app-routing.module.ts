66f801d874d57e24a5cc50720e5109c40cff3283
Bafana-Promise <bafanapromise1@gmail.com>
2023-07-27 23:33:18 +0200

    Added Ui on contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,6 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 const routes: Routes = [
   { path: '', redirectTo: '/giffgaff.com', pathMatch: 'full' }, // redirect to `first-component`
   { path: 'giffgaff.com', component: LandingPageComponent }
-  // {
-  //   path: 'home', component: ContainerComponent, children: [
-  //     { path: '', component: LandingPageComponent },
-  //     { path: 'login', component: LoginComponent },
-  //     { path: 'register', component: RegisterComponent },
-  //   ]
-  // },
-  // { path: 'login', component: LoginComponent },
-  // { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
 
 @NgModule({
