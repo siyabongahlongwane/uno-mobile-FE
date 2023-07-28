3622619e9b9bbbdd4d6e1e9337840c5d43b64be6
Bafana-Promise <bafanapromise1@gmail.com>
2023-07-28 12:55:28 +0200

    Added a functionaliy and redo UI

--- src/app/order-sim/order-sim.component.ts
+++ src/app/order-sim/order-sim.component.ts
@@ -8,33 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class OrderSimComponent implements OnInit {
 
   count = 0;
-  gigObj: {} = {};
+  gigObj:any;
 
   gigsPrices: any [] = [
     {img: "8-pound.svg" ,gig: "6 GB", limit: "Unlimited calls and texts", months: "This 18 month contract gives you the most data for your money and auto-renews each month. EU roaming included up to 5 GB."},
     {img: "10-pound.svg" ,gig: "25 GB", limit: "Unlimited calls and texts", months:"This 18 month contract gives you the most data for your money and auto-renews each month. EU roaming included up to 5 GB."},
     {img: "15-pound.svg" ,gig: "40 GB", limit: "Unlimited calls and texts", months:"This 18 month contract gives you the most data for your money and auto-renews each month. EU roaming included up to 5 GB."},
     {img: "20-pound.svg" ,gig: "120 GB", limit: "Unlimited calls and texts", months:"  This 18 month contract gives you the most data for your money and auto-renews each month. EU roaming included up to 5 GB."},
-    {img: "25-pound.svg" ,gig: "120 GB Alwayson*", limit: "Unlimited calls and texts", months:" This 18 month contract gives you the most data for your money and auto-renews each month. *After 120 GB of data used you'll experience a reduced data speed of 384kbps kbps from 8am to Midnight. You may notice that activities which require high amounts of data, like HD video streaming, will be slower. "},
-    {img: "30-pound.svg" ,gig: "Unlimited data", limit: "Unlimited calls and texts", months:" This 18 month contract gives you the most data for your money and auto-renews each month."}
+    {img: "25-pound.svg" ,gig: "120 GB Alwayson*", limit: "Unlimited calls and texts", months:"This 18 month contract gives you the most data for your money and auto-renews each month. *After 120 GB of data used you'll experience a reduced data speed of 384kbps kbps from 8am to Midnight. You may notice that activities which require high amounts of data, like HD video streaming, will be slower. Traffic Flow applies. EU roaming included up to 5 GB."},
+    {img: "35-pound.svg" ,gig: "Unlimited data", limit: "Unlimited calls and texts", months:" This 18 month contract gives you the most data for your money and auto-renews each month."}
   ]
-  constructor(){}
+  constructor(){
+  }
 
   ngOnInit(): void {
-
+    this.gigObj = this.gigsPrices[this.count]
   }
 
   next(){
     this.count++;
-    this.gigsPrices[this.count]
-    if(this.gigsPrices.length <= this.count){
-      this.gigsPrices[this.count] = 0;
-    }
-    console.log("Break : ", this.gigsPrices[this.count])
+    if(this.gigsPrices.length - 1 == this.count) this.count = 0;
     this.gigObj = this.gigsPrices[this.count];
   }
   previous(){
     this.count--;
+    if(this.count <= 0) this.count = this.gigsPrices.length - 1; 
+    console.log(this.count, this.gigsPrices.length - 1);
+    this.gigObj = this.gigsPrices[this.count];
+    console.log(this.gigObj);
   }
 
 }
